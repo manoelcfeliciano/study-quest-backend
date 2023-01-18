0a63c8d9e87f8c1b1a48afbbae1a5427402991ea
Manoel Feliciano <manoel.feliciano@liberedu.com>
2023-01-18 17:57:20 -0300

    feat: add user domain interfaces

---
+++ src/users/interfaces/user.interface.ts
@@ -0,0 +1,10 @@
+import { Role } from '../enums/role.enum';
+
+export interface UserDomain {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  role: Role;
+  loginAttempts: number;
+}

62607ed94b29a767888503e8638e21318a54829d
Manoel Feliciano <manoel.feliciano@liberedu.com>
2023-01-20 17:43:27 -0300

    feat: add user dtos

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,8 @@
+import { Role } from '../enums/role.enum';
+
+export class CreateUserDto {
+  readonly name: string;
+  readonly email: string;
+  readonly password: string;
+  readonly role?: Role;
+}

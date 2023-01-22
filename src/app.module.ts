f57dbc555b57491f84c14cce45c115f730784453
Manoel Feliciano <manoel.feliciano@liberedu.com>
2023-01-21 22:49:39 -0300

    feat: add jwt config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BcryptService } from './common/hashing/adapters/bcrypt/bcrypt.service';
 import { UsersModule } from './users/users.module';
+import { IamModule } from './iam/iam.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [ConfigModule.forRoot(), UsersModule, IamModule],
   controllers: [AppController],
   providers: [
     AppService,
